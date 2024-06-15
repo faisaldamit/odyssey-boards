abe1fcf6ed6c44f5078ce970294cbf5b6570bfe3
faisaldamit <fbabtayn@gmail.com>
2024-06-16 00:26:26 +0100

    sticky nav bar and new branch

--- homepage/sidebar.js
+++ homepage/sidebar.js
@@ -1,5 +1,6 @@
-//  document.addEventListener("DOMContentLoaded",hidesidebar());
+
 const SideBar = document.getElementById("barrier");
+const navbar = document.getElementById("navibar");
 function showSidebar(){
     SideBar.classList.add("displayBar");
 
@@ -8,3 +9,12 @@ function hideSidebar(){
     SideBar.classList.remove("displayBar");
 
 }
+// let sticky = navbar.offsetTop;
+// window.onscroll = function{sticky_navbar()};
+//  function sticky_navbar(){
+//     if (window.scrollY.pageYOffset >= sticky) {
+//         navbar.classList.add("sticky");
+//       } else {
+//         navbar.classList.remove("sticky");
+//       }
+//  }
