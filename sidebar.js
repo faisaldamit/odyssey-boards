407b1c0dcba4f97bd7745be9cf8a0c80c500ff4b
faisaldamit <fbabtayn@gmail.com>
2024-07-01 10:04:51 +0100

    product page setup

---
+++ sidebar.js
@@ -0,0 +1,20 @@
+
+const SideBar = document.getElementById("barrier");
+// const navbar = document.getElementById("navibar");
+function showSidebar(){
+    SideBar.classList.add("displayBar");
+
+}
+function hideSidebar(){
+    SideBar.classList.remove("displayBar");
+
+}
+// let sticky = navbar.offsetTop;
+// window.onscroll = function{sticky_navbar()};
+//  function sticky_navbar(){
+//     if (window.scrollY.pageYOffset >= sticky) {
+//         navbar.classList.add("sticky");
+//       } else {
+//         navbar.classList.remove("sticky");
+//       }
+//  }
