b018ff71c65be5300dcd1eee5579351f5d9958c1
faisaldamit <fbabtayn@gmail.com>
2024-07-06 21:06:29 +0100

    1st commit of 6/7/24

--- productPage/productAddToCart.js
+++ productPage/productAddToCart.js
@@ -1,66 +1,71 @@
-const pictureOfProduct = document.getElementById("picture");
-const nameOfProduct = document.getElementById("product");
-const priceofmyproduct = document.getElementById("priceOfProduct");
-// const numberBox = document.getElementById("increment-number");
-// let priceOfProduct = document.getElementById("price");
-// let numbers ; 
-const productSB01 = {picture: pictureOfProduct ,
-                    nameOfthisProduct: nameOfProduct,
-                    price :priceofmyproduct
-                    };
-document.addEventListener("DOMContentLoaded",cartMessage());
-
-function cartMessage(){
-
-
-    document.getElementById("checkout-part").innerHTML= '<p class="empty-cart-message">Your cart is currently empty.</p>';
-
+var product = {
+    name: document.getElementById('product').textContent,
+    image: document.getElementById('picture').src,
+    price: document.getElementById('priceOfProduct').textContent
+};
+function addToCart() {
+    
+    
+    localStorage.setItem('cartItem', JSON.stringify(product));
+    window.location.href = '../Cartpage/cartpage.html';
 }
+document.addEventListener('DOMContentLoaded', function() {
+    console.log(document.getElementById('productImage').src);
+});
 
 
-function addToCart(){
-    numbers++;
-    numberBox=numbers;
-    document.getElementById("checkout-part").innerHTML=
-    '<div class="col-sm-6"></div>'+
-     '<div class="col-sm-3"><p class="chackout-part-of-cart">Quantity</p></div>'+
-     '<div class="col-sm-3"><p class="chackout-part-of-cart">Total</p></div>'+
-    '<div class="col-sm-6">'+
-    '<div class="row">'+
-      '<div class="col-sm-6" id="col">'+
-        '<img src="product/product 1.jpg" alt="product1">'+
-        '</div>'+
-        '<div class="col-sm-6" id="col">'+
-        '<h3>product1</h3>'+
-        '</div>'+
-    '</div>'+
-    '</div>'+
-    '<div class="col-sm-3" >'+
-    '<div class="counter-for-products">'+
-    '<button class="button1" type="button" id="minus-button" onclick="decrementing()">-</button>'+
-    '<p id="increment-number">1</p>'+
-    '<button class="button1" type="button" id="plus-button" onclick="incrementing()">+</button>'+
-    '</div>'+
-    '</div>'+
-    '<div class="col-sm-3"id="col">'+
-    '<h3 id="price">199.00</h3>'+
-    '</div>'+
-    '<hr>'+
-    '<div class="row">'+
-    '<div class="col-8"></div>'+
-    '<div class="col-4">'+
-    '<p class="chackout-part-of-cart" >Subtotale :</p>'+
-    '<p class="Subtotale-value" id="Subtotale-value"></p>'+
-    '<button class="btn btn-primary" id="checkout-btn" type="submit">'+
-    'Checkout'+
-    '</button>'+
-    '</div>'+
-    '</div>'+
-    '<hr>'
+function displayCart() {
+    const cartItems = document.getElementById('checkout-part');
+    product = JSON.parse(localStorage.getItem('cartItem'));
+
+    if (product) {
+        const item = document.createElement('div');
+            item.innerHTML = `
+   
+    <div class="col-sm-6"></div>
+        <div class="col-sm-3"><p class="chackout-part-of-cart">Quantity</p></div>
+        <div class="col-sm-3"><p class="chackout-part-of-cart">Total</p></div>
+        <div class="col-sm-6">
+    <div class="row">
+        <div class="col-sm-6" id="col">
+        <img src="${product.image}" alt="${product.name}">
+        </div>
+        <div class="col-sm-6" id="col">
+        <h3>${product.name}</h3>
+            </div>
+        </div>
+        </div>
+        <div class="col-sm-3" >
+        <div class="counter-for-products">
+        <button class="button1" type="button" id="minus-button" onclick="decrementing()">-</button>
+        <p id="increment-number">1</p>
+        <button class="button1" type="button" id="plus-button" onclick="incrementing()">+</button>
+        </div>
+        </div>
+        <div class="col-sm-3"id="col">
+        <h3 id="price">${product.price}</h3>
+        </div>
+        <hr>
+        <div class="row">
+        <div class="col-8"></div>
+        <div class="col-4">
+        <p class="chackout-part-of-cart" >Subtotale :</p>
+        <p class="Subtotale-value" id="Subtotale-value"></p>
+        <button class="btn btn-primary" id="checkout-btn" type="submit">
+        Checkout
+        </button>
+        </div>
+        </div>
+        <hr>`
     ;
+    cartItems.appendChild(item);
+     } else{
+        cartItems.innerHTML=`<p class="empty-cart-message">Your cart is currently empty.</p>`;
+     }
+    }
+
+    // Check if we're on the cart page
+if (window.location.pathname.includes('../Cartpage/cartpage.html')) {
+    displayCart();
 }
-// <!-- <div class="row">
-//  <div class="col-sm-6"></div>
-//     <div class="col-sm-3"><p class="chackout-part-of-cart">Quantity</p></div>
-//     <div class="col-sm-3"><p class="chackout-part-of-cart">Total</p></div>
-//  </div> -->
+//    document.addEventListener("DOMContentLoaded",displayCart());
